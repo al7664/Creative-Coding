16eedab47728eb3e85a9ac8fab2b3b5a9a7eccb1
al7664 <al7664@nyu.edu>
2024-03-12 23:41:49 +0800

    New

--- cclabAtlas/js/sketch.js
+++ cclabAtlas/js/sketch.js
@@ -1,118 +1,54 @@
 let x, y;
-let speed = 5;
-let prevPositions = [];
-let cloudX1 = 0;
-let cloudY1 = 400;
-let cloudX2 = 400;
-let cloudY2 = 220;
-let cloudX3 = -400;
-let cloudY3 = 0;
-let cloudSpeed = 1;
 let creatureColor = 150;
+let prevPositions = [];
+let pollutionClouds = [];
+let canMove = true;
 
 function setup() {
   let canvas = createCanvas(800, 500);
-  canvas.id("p5canvas")
+  canvas.parent("p5canvas")
   x = width / 2;
   y = height / 2;
   background(173, 216, 230);
+  resetCanvas();
 }
 
 function draw() {
   background(173, 216, 230);
 
-  // Clouds
-  fill(255);
-  noStroke();
-  ellipse(cloudX1, cloudY1, 100, 50);
-  ellipse(cloudX1 + 50, cloudY1 + 20, 200, 75);
-  ellipse(cloudX1 + 100, cloudY1, 150, 100);
-  ellipse(cloudX1 + 150, cloudY1 + 20, 200, 100);
-  ellipse(cloudX1 + 200, cloudY1, 200, 100);
-  ellipse(cloudX1 + 250, cloudY1 + 20, 150, 100);
-  cloudX1 += cloudSpeed;
-  if (cloudX1 > width) {
-    cloudX1 = -200;
-  }
+  // Draw Clouds
+  drawClouds();
 
-  ellipse(cloudX2, cloudY2, 200, 100);
-  ellipse(cloudX2 + 50, cloudY2 + 20, 50, 50);
-  ellipse(cloudX2 + 100, cloudY2, 150, 100);
-  ellipse(cloudX2 + 150, cloudY2 + 20, 200, 100);
-  ellipse(cloudX2 + 200, cloudY2, 200, 75);
-  ellipse(cloudX2 + 250, cloudY2 + 20, 100, 70);
-  cloudX2 += cloudSpeed;
-  if (cloudX2 > width) {
-    cloudX2 = -200;
-  }
+  // Pollution Clouds
+  for (let i = 0; i < pollutionClouds.length; i++) {
+    let cloud = pollutionClouds[i];
+    fill(100, 100, 100, 100);
+    ellipse(cloud.x, cloud.y, cloud.size);
 
-  ellipse(cloudX3, cloudY3, 150, 50);
-  ellipse(cloudX3 + 50, cloudY3 + 20, 50, 50);
-  ellipse(cloudX3 + 100, cloudY3, 250, 75);
-  ellipse(cloudX3 + 150, cloudY3 + 20, 200, 100);
-  ellipse(cloudX3 + 200, cloudY3, 180, 100);
-  ellipse(cloudX3 + 250, cloudY3 + 20, 250, 50);
-  cloudX3 += cloudSpeed;
-  if (cloudX3 > width) {
-    cloudX3 = -200;
-  }
-
-  // Arrow Keys
-  if (keyIsPressed) {
-    if (keyCode === UP_ARROW && y - speed >= 0) {
-      y -= speed;
-    } else if (keyCode === DOWN_ARROW && y + speed <= height) {
-      y += speed;
-    } else if (keyCode === LEFT_ARROW && x - speed >= 0) {
-      x -= speed;
-    } else if (keyCode === RIGHT_ARROW && x + speed <= width) {
-      x += speed;
-    } else if (keyCode === 32) {
-      restartCanvas();
+    // Update cloud position
+    cloud.x += cloud.speed;
+    if (cloud.x > width + cloud.size) {
+      cloud.x = -cloud.size;
     }
   }
-  // Ellipse Size
-  let ellipseWidth = sin(frameCount * 0.1) * 30 + 50;
-  let ellipseHeight = sin(frameCount * 0.1) * 30 + 50;
-  let alpha = map(ellipseWidth, 50, 80, 0, 255);
-  fill(creatureColor, alpha);
-  ellipse(x, y, ellipseWidth, ellipseHeight);
-  prevPositions.push({ x: x, y: y });
 
-  // Tail
-  for (let i = 0; i < prevPositions.length; i++) {
-    let pos = prevPositions[i];
-    let circleSize = sin(frameCount * 0.1 - i * 0.1) * 30 + 50;
-    let circleAlpha = map(circleSize, 50, 80, 0, 255);
-    fill(creatureColor, circleAlpha);
-    ellipse(pos.x, pos.y, circleSize, circleSize);
+  // Periodic Cloud
+  if (frameCount % 60 === 0) {
+    pollutionClouds.push({
+      x: random(width),
+      y: random(height),
+      size: random(30, 100),
+      speed: random(1, 3),
+    });
   }
 
-  // Scarier Eyes
-  let eyeSize = 25;
-  let eyeOffset = 20;
-  fill(255);
-  ellipse(x - eyeOffset, y - eyeOffset, eyeSize * 1.2, eyeSize * 1.2);
-  ellipse(x + eyeOffset, y - eyeOffset, eyeSize * 1.2, eyeSize * 1.2);
-  fill(0);
-  ellipse(x - eyeOffset, y - eyeOffset, eyeSize * 0.8, eyeSize * 0.8);
-  ellipse(x + eyeOffset, y - eyeOffset, eyeSize * 0.8, eyeSize * 0.8);
+  // Draw Creature
+  drawCreature();
 
-  // Eyebrows
-  fill(0);
-  noStroke();
-
-  push();
-  translate(x - eyeOffset - 12, y - eyeOffset - 25);
-  rotate(radians(45));
-  rect(0, 0, 25, 8, 3);
-  pop();
-
-  push();
-  translate(x + eyeOffset - 13, y - eyeOffset - 25);
-  rotate(radians(-45));
-  rect(-10, 17, 25, 8, 3);
-  pop();
+  // Arrow Keys
+  if (canMove && keyIsPressed) {
+    handleArrowKeys(5); // Adjust speed as needed
+  }
 
   // Change Color
   if (mouseIsPressed) {
@@ -122,22 +58,121 @@ function draw() {
     }
   }
 
-  drawInstructions(); 
+  drawInstructions();
 }
 
-function restartCanvas() {
-  x = width / 2;
-  y = height / 2;
-  creatureColor = 150;
-  prevPositions = [];
+function drawClouds() {
+  drawMovingCloud(0, 400, 0.5);
+  drawMovingCloud(400, 220, 0.3);
+  drawMovingCloud(-400, 0, 0.7);
+}
+
+function drawMovingCloud(cloudX, cloudY, speed) {
+  cloudX += (frameCount * speed) % width; //
+  fill(255);
+  noStroke();
+  ellipse(cloudX, cloudY, 100, 50);
+  ellipse(cloudX + 50, cloudY + 20, 200, 75);
+  ellipse(cloudX + 100, cloudY, 150, 100);
+  ellipse(cloudX + 150, cloudY + 20, 200, 100);
+  ellipse(cloudX + 200, cloudY, 200, 100);
+  ellipse(cloudX + 250, cloudY + 20, 150, 100);
+}
+
+function drawCreature() {
+  // Ellipse Size
+  let ellipseWidth = sin(frameCount * 0.1) * 30 + 50;
+  let ellipseHeight = sin(frameCount * 0.1) * 30 + 50;
+  let alpha = map(ellipseWidth, 50, 80, 0, 255);
+  fill(creatureColor, alpha);
+  ellipse(x, y, ellipseWidth, ellipseHeight);
+  prevPositions.push({ x: x, y: y });
+
+  // Draw Tail
+  drawTail();
+
+  // Draw Eyes
+  drawEyes();
+
+  // Draw Eyebrows
+  drawEyebrows();
+}
+
+function drawTail() {
+  for (let i = 0; i < prevPositions.length; i++) {
+    let pos = prevPositions[i];
+    let circleSize = sin(frameCount * 0.1 - i * 0.1) * 30 + 50;
+    let circleAlpha = map(circleSize, 50, 80, 0, 255);
+    fill(creatureColor, circleAlpha);
+    ellipse(pos.x, pos.y, circleSize, circleSize);
+  }
+}
+
+function drawEyes() {
+  let eyeSize = 25;
+  let eyeOffset = 20;
+  fill(255);
+  ellipse(x - eyeOffset, y - eyeOffset, eyeSize * 1.2, eyeSize * 1.2);
+  ellipse(x + eyeOffset, y - eyeOffset, eyeSize * 1.2, eyeSize * 1.2);
+  fill(0);
+  ellipse(x - eyeOffset, y - eyeOffset, eyeSize * 0.8, eyeSize * 0.8);
+  ellipse(x + eyeOffset, y - eyeOffset, eyeSize * 0.8, eyeSize * 0.8);
+}
+
+function drawEyebrows() {
+  fill(0);
+  noStroke();
+
+  push();
+  translate(x - 20 - 12, y - 20 - 25);
+  rotate(radians(45));
+  rect(0, 0, 25, 8, 3);
+  pop();
+
+  push();
+  translate(x + 20 - 13, y - 20 - 25);
+  rotate(radians(-45));
+  rect(-10, 17, 25, 8, 3);
+  pop();
+}
+
+function handleArrowKeys(speed) {
+  if (keyIsDown(UP_ARROW) && y - speed >= 0) {
+    y -= speed;
+  } else if (keyIsDown(DOWN_ARROW) && y + speed <= height) {
+    y += speed;
+  } else if (keyIsDown(LEFT_ARROW) && x - speed >= 0) {
+    x -= speed;
+  } else if (keyIsDown(RIGHT_ARROW) && x + speed <= width) {
+    x += speed;
+  }
+}
+
+function resetCanvas() {
   background(173, 216, 230);
+  prevPositions = [];
+  pollutionClouds = [];
+  creatureColor = 150;
+  canMove = true;
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    resetCanvas();
+  }
 }
 
 function drawInstructions() {
-  textSize(20);
+  textSize(14);
   fill(0);
-  textAlign(CENTER);
-  text("Utilize the arrows to spread the pollution Smogo", width / 2, 30);
-  text("Click the mouse to make it darker", width / 2, 60);
-  text("Press spacebar to restart", width / 2, 90);
+  textAlign(LEFT, TOP);
+  text("Utilize the arrows and mouse", 10, 10);
+  text("Press spacebar to reset canvas", 10, 30);
+}
+
+function mouseClicked() {
+  creatureColor -= 5;
+  if (creatureColor < 0) {
+    creatureColor = 0;
+  }
 }
